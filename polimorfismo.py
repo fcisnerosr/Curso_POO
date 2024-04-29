d3972c92b76ca565a8ab67035b7a7c2104c1bbae
Paco <fcisnerosr@outlook.es>
2024-04-29 08:41:35 -0600

    diferentes clases de polimorfismo

--- polimorfismo.py
+++ polimorfismo.py
@@ -5,14 +5,19 @@ class Gato():
 class Perro():
     def sonido(self):
         return 'guau'
-    
+
+# Definición de objetos
 gato1 = Gato()
 perro1= Perro()
 
+# polimorfismo de métodos
 print(gato1.sonido())
 print(perro1.sonido())
 
+
+# polimorfismo de funcion
 def hacer_sonido(animal):
     print(animal.sonido())
     
 hacer_sonido(gato1)
+hacer_sonido(perro1)
